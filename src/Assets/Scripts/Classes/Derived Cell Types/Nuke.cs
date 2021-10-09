303c4e63e5e94bb177d7ee0b28b08358b27d6170
youtissoum <youtissoum@outlook.fr>
2021-10-09 11:37:24 +0200

    revamped nuke script
    i completely remade the script for the nuke and added the textures for the flipper cell

--- src/Assets/Scripts/Classes/Derived Cell Types/Nuke.cs
+++ src/Assets/Scripts/Classes/Derived Cell Types/Nuke.cs
@@ -4,10 +4,7 @@ using UnityEngine;
 
 public class Nuke : TrackedCell
 {
-    bool runThisTick = true;
-
-    public bool isActive()
-    {
+    public bool isActive() {
         int offsetX = 0;
         int offsetY = 0;
 
@@ -27,26 +24,22 @@ public class Nuke : TrackedCell
                 break;
         }
         //Array index error prevention
-        if (this.position.x + -1 < 0 || this.position.y + -1 < 0)
+        if (this.position.x - offsetX < 0 || this.position.y - offsetY < 0)
             return false;
-        if (this.position.x + 1 >= CellFunctions.gridWidth || this.position.y + 1 >= CellFunctions.gridHeight)
+        if (this.position.x - offsetX >= CellFunctions.gridWidth || this.position.y - offsetY >= CellFunctions.gridHeight)
             return false;
-        if (this.position.x + -2 < 0 || this.position.y + -2 < 0)
+        if (this.position.x + offsetX < 0 || this.position.y + offsetY < 0)
             return false;
-        if (this.position.x + 2 >= CellFunctions.gridWidth || this.position.y + 2 >= CellFunctions.gridHeight)
+        if (this.position.x + offsetX >= CellFunctions.gridWidth || this.position.y + offsetY >= CellFunctions.gridHeight)
             return false;
-
-        if (this.position.x < 0 || this.position.y < 0)
-            return false;
-        if (this.position.x >= CellFunctions.gridWidth || this.position.y >= CellFunctions.gridHeight)
-            return false;
+        //If we don't have a refrence cell return
+        /*if (CellFunctions.cellGrid[(int)this.position.x - offsetX, (int)this.position.y - offsetY] == null)
+            return false;*/
         return true;
     }
 
     public override void Step()
     {
-        this.runThisTick = !this.runThisTick;
-
         //Subract to find refrence, add to find target
         int offsetX = 0;
         int offsetY = 0;
@@ -67,93 +60,61 @@ public class Nuke : TrackedCell
                 break;
         }
         //Array index error prevention
-        if (this.position.x + -1 < 0 || this.position.y + -1 < 0)
+        if (this.position.x - offsetX < 0 || this.position.y - offsetY < 0)
+            return;     
+        if (this.position.x - offsetX >= CellFunctions.gridWidth || this.position.y - offsetY >= CellFunctions.gridHeight)
             return;
-        if (this.position.x + 1 >= CellFunctions.gridWidth || this.position.y + 1 >= CellFunctions.gridHeight)
+        if (this.position.x + offsetX < 0 || this.position.y + offsetY < 0)
+            return;
+        if (this.position.x + offsetX >= CellFunctions.gridWidth || this.position.y + offsetY >= CellFunctions.gridHeight)
             return;
 
-        if (this.position.x < 0 || this.position.y < 0)
-            return;
-        if (this.position.x >= CellFunctions.gridWidth || this.position.y >= CellFunctions.gridHeight)
-            return;
+        //If we don't have a refrence cell return
+        /*if (CellFunctions.cellGrid[(int)this.position.x - offsetX, (int)this.position.y - offsetY] == null)
+            return;*/
         //If there is a cell in our way push it :3
-        if (CellFunctions.cellGrid[(int)this.position.x + -1, (int)this.position.y + 0] != null)
+        if (CellFunctions.cellGrid[(int)this.position.x + offsetX, (int)this.position.y + offsetY] != null)
         {
             //if (CellFunctions.cellGrid[(int)this.position.x + offsetX, (int)this.position.y + offsetY].cellType == CellType_e.TRASH)
             //    return;
 
-            (bool, bool) pushResult = CellFunctions.cellGrid[(int)this.position.x + -1, (int)this.position.y + 0].Push(Direction_e.LEFT, 1);
-            if (pushResult.Item2 || !pushResult.Item1)
-                return;
-        }
-        else if(CellFunctions.cellGrid[(int)this.position.x + 1, (int) this.position.y + 0] != null)
-        {
-            //if (CellFunctions.cellGrid[(int)this.position.x + offsetX, (int)this.position.y + offsetY].cellType == CellType_e.TRASH)
-            //    return;
-
-            (bool, bool) pushResult = CellFunctions.cellGrid[(int)this.position.x + 1, (int)this.position.y + 0].Push(Direction_e.RIGHT, 1);
-            if (pushResult.Item2 || !pushResult.Item1)
-                return;
-        }
-        else if (CellFunctions.cellGrid[(int)this.position.x + 0, (int)this.position.y + 1] != null)
-        {
-            //if (CellFunctions.cellGrid[(int)this.position.x + offsetX, (int)this.position.y + offsetY].cellType == CellType_e.TRASH)
-            //    return;
-
-            (bool, bool) pushResult = CellFunctions.cellGrid[(int)this.position.x + 0, (int)this.position.y + 1].Push(Direction_e.UP, 1);
-            if (pushResult.Item2 || !pushResult.Item1)
-                return;
-        }
-        else if (CellFunctions.cellGrid[(int)this.position.x + 0, (int)this.position.y + -1] != null)
-        {
-            //if (CellFunctions.cellGrid[(int)this.position.x + offsetX, (int)this.position.y + offsetY].cellType == CellType_e.TRASH)
-            //    return;
-
-            (bool, bool) pushResult = CellFunctions.cellGrid[(int)this.position.x + 0, (int)this.position.y + -1].Push(Direction_e.DOWN, 1);
+            (bool, bool) pushResult = CellFunctions.cellGrid[(int)this.position.x + offsetX, (int)this.position.y + offsetY].Push(this.getDirection(), 1);
             if (pushResult.Item2 || !pushResult.Item1)
                 return;
         }
 
-        if (this.runThisTick)
-        {
-            AudioManager.i.PlaySound(GameAssets.i.place);
-            Cell newCellLeft = GridManager.instance.SpawnCell(
-                        CellType_e.NUKE,
-                        new Vector2(this.position.x + -1, this.position.y + 0),
-                        Direction_e.RIGHT,
-                        true
-                        );
-            newCellLeft.oldPosition = this.position;
-            newCellLeft.generated = true;
-
-            Cell newCellRight = GridManager.instance.SpawnCell(
-                        CellType_e.NUKE,
-                        new Vector2(this.position.x + 1, this.position.y + 0),
-                        Direction_e.RIGHT,
-                        true
-                        );
-            newCellRight.oldPosition = this.position;
-            newCellRight.generated = true;
-
-            Cell newCellUp = GridManager.instance.SpawnCell(
-                        CellType_e.NUKE,
-                        new Vector2(this.position.x + 0, this.position.y + 1),
-                        Direction_e.RIGHT,
-                        true
-                        );
-            newCellUp.oldPosition = this.position;
-            newCellUp.generated = true;
-
-            Cell newCellDown = GridManager.instance.SpawnCell(
-                        CellType_e.NUKE,
-                        new Vector2(this.position.x + 0, this.position.y + -1),
-                        Direction_e.RIGHT,
-                        true
-                        );
-            newCellDown.oldPosition = this.position;
-            newCellDown.generated = true;
-
-            // print("Tick");
-        }
+        AudioManager.i.PlaySound(GameAssets.i.place);
+        Cell newCell1 = GridManager.instance.SpawnCell(
+            CellType_e.NUKE,
+            new Vector2((int)this.position.x + 1, (int)this.position.y + 0),
+            Direction_e.UP,
+            true
+            );
+        newCell1.oldPosition = this.position;
+        newCell1.generated = true;
+        Cell newCell2 = GridManager.instance.SpawnCell(
+            CellType_e.NUKE,
+            new Vector2((int)this.position.x - 1, (int)this.position.y - 0),
+            Direction_e.UP,
+            true
+            );
+        newCell2.oldPosition = this.position;
+        newCell2.generated = true;
+        Cell newCell3 = GridManager.instance.SpawnCell(
+            CellType_e.NUKE,
+            new Vector2((int)this.position.x + 0, (int)this.position.y + 1),
+            Direction_e.UP,
+            true
+            );
+        newCell3.oldPosition = this.position;
+        newCell3.generated = true;
+        Cell newCell4 = GridManager.instance.SpawnCell(
+            CellType_e.NUKE,
+            new Vector2((int)this.position.x - 0, (int)this.position.y - 1),
+            Direction_e.UP,
+            true
+            );
+        newCell4.oldPosition = this.position;
+        newCell4.generated = true;
     }
 }
